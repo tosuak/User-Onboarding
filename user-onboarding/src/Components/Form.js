a3d190ed729a5af816652a81a8173443348a34c5
Tafiqul <demianviotto@gmail.com>
2021-12-15 19:35:07 -0500

    finished on form.js. Working on styling

--- user-onboarding/src/Components/Form.js
+++ user-onboarding/src/Components/Form.js
@@ -1,3 +1,89 @@
-export default function Form(props) {
+import React from "react";
 
+export default function Form(props) {
+const { values, submit, change, disabled, errors } = props
+
+const onSubmit = evt => {
+    evt.preventDefault()
+    submit()
+}
+
+const onChange = evt => {
+    const { name, value, checked, type } = evt.target
+    const valueToUse = type === 'checkbox' ? checked : value;
+    change(name, valueToUse)
+}
+
+return (
+    <form className="form container" onSubmit={onSubmit}>
+        <div className="form-group submit">
+            <h2>Add a User</h2>
+            <button disabled={disabled}>Submit</button>
+            <div className="errors">
+                <div>{errors.firstName}</div>
+                <div>{errors.lastName}</div>
+                <div>{errors.username}</div>
+                <div>{errors.email}</div>
+                <div>{errors.password}</div>
+            </div>
+        </div>
+
+        <div className="form-group inputs">
+            <h4>General Information</h4>
+            <label>First Name
+                <input 
+                    value={values.firstName}
+                    onChange={onChange}
+                    name="firstName"
+                    type='text'
+                />
+            </label>
+
+            <label>Last Name
+                <input 
+                    value={values.lastName}
+                    onChange={onChange}
+                    name="lastName"
+                    type='text'
+                />
+            </label>
+
+            <label>Username
+                <input 
+                    value={values.username}
+                    onChange={onChange}
+                    name="username"
+                    type='text'
+                />
+            </label>
+
+            <label>Email
+                <input 
+                    value={values.email}
+                    onChange={onChange}
+                    name="email"
+                    type='email'
+                />
+            </label>
+
+            <label>Password
+                <input 
+                    value={values.password}
+                    onChange={onChange}
+                    name="password"
+                    type='password'
+                />
+            </label>
+
+            <label>Terms of Services
+                <input 
+                    type='checkbox'
+                    name='terms'
+                    checked={values.terms}
+                    onChange={onChange}
+                />
+            </label>
+        </div>
+    </form>
+)
 };
--- user-onboarding/src/Components/User.js
+++ user-onboarding/src/Components/User.js
@@ -1,3 +1,18 @@
-export default function User() {
-    
+import React from 'react'
+
+function User({ details }) {
+  if (!details) {
+    return <h3>Working fetching your friend&apos;s details...</h3>
+  }
+
+  return (
+    <div className='User container'>
+      <h2>{details.username}</h2>
+      <p>First Name: {details.firstName}</p>
+      <p>Last Name: {details.lastName}</p>
+      <p>Email: {details.email}</p>
+    </div>
+  )
 }
+
+export default User
